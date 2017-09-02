82d5f10c7e71352c2a1cb418056c89bb2b906912
huxuexue <1711356156@qq.com>
2017-09-02 23:43:32 +0800

    angular路由 完成 Signed-off-by: huxuexue <1711356156@qq.com>

--- js/x-js/about.js
+++ js/x-js/about.js
@@ -7,11 +7,10 @@ $(function(){
 
 
 
-// 路由跳转
+// 关于我们 路由跳转
 var app=angular.module('app',['ui.router']); 
 app.config(function($stateProvider,$urlRouterProvider){
     $urlRouterProvider.otherwise("/gywm");
-    // $urlRouterProvider.otherwise("/jrwm/jrwmInfo");
     $stateProvider.state('gywm',{
         url:'/gywm',
         templateUrl:'gywm.html'
@@ -42,7 +41,45 @@ app.config(function($stateProvider,$urlRouterProvider){
     }).state('jrwm.jrwmInfo2',{
         url:'/jrwmInfo2',
         templateUrl:'jrwmInfo2.html'
+    }).state('lxwm',{
+        url:'/lxwm',
+        templateUrl:'lxwm.html'
     })
 })
 
+
+
+//投资者关系  路由跳转
+var app2=angular.module('app2',['ui.router']); 
+app2.config(function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise("/gsgg");
+    $stateProvider.state('gsgg',{
+        url:'/gsgg',
+        templateUrl:'gsgg.html',
+        controller:function ($state) {
+            $state.go('gsgg.gsgglist1');
+        }
+    }).state('gsgg.gsgglist1',{
+        url:'/gsgglist1',
+        templateUrl:'gsgglist1.html'
+    }).state('gsgg.gsgglist2',{
+        url:'/gsgglist2',
+        templateUrl:'gsgglist2.html'
+    }).state('gszc',{
+        url:'/gszc',
+        templateUrl:'gszc.html',
+        controller:function ($state) {
+            $state.go('gszc.gszclist');
+        }
+    }).state('gszc.gszclist',{
+        url:'/gszclist',
+        templateUrl:'gszclist.html'
+    }).state('gszc.gszclist2',{
+        url:'/gszclist2',
+        templateUrl:'gszclist2.html'
+    }).state('gpxx',{
+        url:'/gpxx',
+        templateUrl:'gpxx.html'
+    })
+})
 
